99e54cc659ee446a1daf7027c31d0ea8ee4323e2
Salavat <salavatfederal@gmail.com>
2023-03-06 19:05:34 +0300

    inital

---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Product {
+    protected int id;
+    protected String name;
+    protected int price;
+
+    public Product(int id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean matches(String search) {
+
+        return getName().contains(search);
+
+    }
+}
